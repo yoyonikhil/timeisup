db1dba3a94a97a2f7ccf92966bab9eaa56da4497
yoyonikhil <40619915+yoyonikhil@users.noreply.github.com>
2019-12-29 13:35:28 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -121,8 +121,8 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"My Master Has Been Gone For {afk_since}\nWhere He Is: ONLY GOD KNOWS " + \
-            f"\n\n__I promise I'll back in a few hours__\n**REASON**: {reason}" \
+        message_to_reply = f"My Master Has Been Gone For {afk_since}\nWhere He Is: BUSY IN STUDY " + \
+            f"\n\n__I promise I'll back in a 50SEC__\n**REASON**: {reason}" \
             if reason \
             else f"**Important Notice**\n\n[This User Is Ded Forever...](https://telegra.ph//file/a53fa950ff31781d5930a.jpg) "
         msg = await event.reply(message_to_reply)
