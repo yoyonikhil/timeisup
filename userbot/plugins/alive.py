4dcdec0659b4eeb0578bf07c3430247a2b1ebbe8
yoyonikhil <40619915+yoyonikhil@users.noreply.github.com>
2019-12-31 19:19:29 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,6 +12,6 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**`Jis race se mujhe nikaalne ki baat kar rahe hai yeh bewakoof ... woh nahi jaante hai ... us race ka sikandar main hoon  ψ(｀∇´)ψ`**\n\n"
-                     "`milte hai crew challenge me yad rakhna Nikhil \nINDIA IS GREAT\n`Crew Challenge: #4 Rank holder\n\nAlways with you, my master!\n`"
+                     "`milte hai crew challenge me yad rakhna Nikhil \n\nHAPPY NEW YEAR😃\n`Crew Challenge: #4 Rank holder😁\n\nAlways with you, my master!\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n"
                      "[Deploy this userbot Now](https://github.com/yoyonikhil/timeisup)")
